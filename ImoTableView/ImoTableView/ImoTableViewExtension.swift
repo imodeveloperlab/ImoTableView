bad91fbdc5a6a85ab1f48c044437245d9a622bb6
Borinschi Ivan <borinschiivan@Borinschis-MacBook-Pro.local>
2017-05-09 19:10:05 +0300

    add new extension, to add possibility to auto scroll table when keyboard is shown on screen.

---
+++ ImoTableView/ImoTableView/ImoTableViewExtension.swift
@@ -0,0 +1,9 @@
+//
+//  ImoTableViewExtension.swift
+//  ImoTableView
+//
+//  Created by Borinschi Ivan on 5/9/17.
+//  Copyright © 2017 Imodeveloperlab. All rights reserved.
+//
+
+import Foundation
