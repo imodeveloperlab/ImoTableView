8710540560ebd41e421b8fa4957a7325cf862e7b
Borinschi Ivan <borinschiivan@Borinschis-MacBook-Pro.local>
2017-05-10 16:50:20 +0300

    ...

--- ImoTableViewExample/ImoTableViewExampleUITests/ImoTableViewExampleUITests.swift
+++ ImoTableViewExample/ImoTableViewExampleUITests/ImoTableViewExampleUITests.swift
@@ -1,6 +1,6 @@
 //
-//  ImoTableViewUITests.swift
-//  ImoTableViewUITests
+//  ImoTableViewExampleUITests.swift
+//  ImoTableViewExampleUITests
 //
 //  Created by Borinschi Ivan on 5/10/17.
 //  Copyright © 2017 Imodeveloperlab. All rights reserved.
@@ -8,12 +8,13 @@
 
 import XCTest
 
-class ImoTableViewUITests: XCTestCase {
+class ImoTableViewExampleUITests: XCTestCase {
         
     override func setUp() {
         super.setUp()
         
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
@@ -26,5 +27,4 @@ class ImoTableViewUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
 }
